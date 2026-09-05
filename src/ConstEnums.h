9a49ea495a6050dbe265c0f11823d3c1c6cbd9d9
tringlemthlth4-prog <tringlemthlth4@gmail.com>
2026-09-05 19:21:21 +0300

    Add DIALOG_CHANGDIFFICULTY to ConstEnums

--- src/ConstEnums.h
+++ src/ConstEnums.h
@@ -320,6 +320,7 @@ enum Dialogs : int32_t
 	DIALOG_PURCHASE_PACKET_SLOT,                // 50
 	DIALOG_ZOMBATAR_TOS,                        // 51: terms of service
 	DIALOG_ZOMBATAR_DELETE,                     // 52
+	DIALOG_CHANGDIFFICULTY,                     // 53
 	NUM_DIALOGS
 };
 enum DebugTextMode : int32_t
