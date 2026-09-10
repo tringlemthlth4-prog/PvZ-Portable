a9007c99f29b3ffef86d03cab4a84fbcef7b34c9
tringlemthlth4-prog <tringlemthlth4@gmail.com>
2026-09-10 18:25:48 +0300

    Remove copyright and add DoCheatMenu method
    Removed copyright notice and added DoCheatMenu method.

--- src/LawnApp.h
+++ src/LawnApp.h
@@ -1,24 +1,3 @@
-/*
- * Copyright (C) 2026 Zhou Qiankang <wszqkzqk@qq.com>
- *
- * SPDX-License-Identifier: LGPL-3.0-or-later
- *
- * This file is part of PvZ-Portable.
- *
- * PvZ-Portable is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * PvZ-Portable is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with PvZ-Portable. If not, see <https://www.gnu.org/licenses/>.
- */
-
 #ifndef __LAWNAPP_H__
 #define __LAWNAPP_H__
 
@@ -163,6 +142,7 @@ public:
 	void							FinishUserDialog(bool isYes);
 	void							DoCreateUserDialog();
 	void							DoCheatDialog();
+	void							DoCheatMenu();
 	void							FinishCheatDialog(bool isYes);
 	void							FinishCreateUserDialog(bool isYes);
 	void							FinishChangeDifficultyDialog(bool isHard);
