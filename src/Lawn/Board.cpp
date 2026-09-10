ab5a13f46f3e1b66f23cfa7fe0a90b8667ecc93b
tringlemthlth4-prog <tringlemthlth4@gmail.com>
2026-09-10 16:19:57 +0300

    Remove copyright and adjust sun money for hard mode
    Removed copyright notice and updated sun money calculation for hard mode.

--- src/Lawn/Board.cpp
+++ src/Lawn/Board.cpp
@@ -1,24 +1,3 @@
-/*
- * Copyright (C) 2026 Zhou Qiankang <wszqkzqk@qq.com>
- *
- * SPDX-License-Identifier: LGPL-3.0-or-later
- *
- * This file is part of PvZ-Portable.
- *
- * PvZ-Portable is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * PvZ-Portable is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with PvZ-Portable. If not, see <https://www.gnu.org/licenses/>.
- */
-
 #include <time.h>
 #include <algorithm>
 #include <SDL.h>
@@ -1368,7 +1347,7 @@ void Board::InitLevel()
 	}
 	else if (mApp->IsIZombieLevel())
 	{
-		mSunMoney = 150;
+		mSunMoney = mHardMode ? 300 : 150;
 	}
 	else if (mApp->IsFirstTimeAdventureMode() && mLevel == 1)
 	{
